b58bd1a0f0603ca3203f7acf1cf4cc9b482eb388
ohashiyukio <ohashiyukio222@gmail.com>
2025-01-08 21:16:19 +0900

    削除、編集機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,7 +12,24 @@ class TweetsController < ApplicationController
     redirect_ti '/'
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
+
   private
+
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
   end  
